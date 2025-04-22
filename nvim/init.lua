9662bb01e67cd04429002539e5187390e21430ab
David Bucher <d.bucher97@gmail.com>
2025-04-22 14:30:17 +0200

    switched to none-ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -48,8 +48,8 @@ require("lazy").setup("plugins", opts)
 vim.cmd([[colorscheme onedark]])
 
 
-vim.keymap.set("n", "<C-j>", "<C-w>j", {silent = true})
-vim.keymap.set("n", "<C-k>", "<C-w>k", {silent = true})
-vim.keymap.set("n", "<C-l>", "<C-w>l", {silent = true})
-vim.keymap.set("n", "<C-h>", "<C-w>h", {silent = true})
+-- vim.keymap.set("n", "<C-j>", "<C-w>j", {silent = true})
+-- vim.keymap.set("n", "<C-k>", "<C-w>k", {silent = true})
+-- vim.keymap.set("n", "<C-l>", "<C-w>l", {silent = true})
+-- vim.keymap.set("n", "<C-h>", "<C-w>h", {silent = true})
 vim.keymap.set("n", "<leader><tab>", ":b#<cr>", {silent = true})
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -30,7 +30,6 @@ return {
     -- { "lukas-reineke/indent-blankline.nvim" },
     {
         "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
         cmd = { "Neotree" },
         keys = {
             { "<Tab>", "<cmd>Neotree toggle<cr>" },
@@ -54,5 +53,23 @@ return {
         config = function()
             require("gitsigns").setup()
         end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+            "TmuxNavigatorProcessList",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            -- { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
     },
 }
